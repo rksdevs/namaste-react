37cf42319b2bc4e6f5c9b533841eef1b79350994
Rakesh <mailto.dev.rakesh@gmail.com>
2023-07-18 21:11:30 +0530

    laying the foundation- ch 03

--- Inception/App.js
+++ Inception/App.js
@@ -1,9 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// chapter -3 starts//
 
-const parent = React.createElement("div", {id: "parent", xyz: "abc "}, [React.createElement("div", {id: "child", xyz: "abc "}, "I'm child 1"), React.createElement("div", {id: "child", xyz: "abc"}, "I'm child 2")]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+    //creating a react element
+    const Heading = () => (<h1 id="jsxHeading">Namaste JSX</h1>);
+    const paragraph = <p>This is a paragraph</p>
+    const number = 100; 
 
-// console.log(heading);
-root.render(parent);        
+
+    // React Functional Components
+    const HeadingComponent = () => {
+        return (<div id="container">
+
+                {/* Inserting a react component */}
+                <Heading />
+
+                {/* Inserting a react element  */}
+                {paragraph}
+
+                {/* performing JS evaluation or JS codes inside JSX */}
+                {number * 20}
+                {Heading ()}
+
+                {/* Regular JSX code */}
+                <h1>Namaste React Functional component</h1>
+            </div>)
+    }
+
+    // const HeadingComponent2 = () => (<div id="container">
+    //             <Heading />
+    //             <hi>Namaste React Functional component 2</hi>
+    //         </div>);
+
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(<HeadingComponent />);
+
+
+//chapter -3 ends//
+
+
+// chapter -1 - starts//
+// const parent = React.createElement("div", {id: "parent", xyz: "abc "}, [React.createElement("div", {id: "child", xyz: "abc "}, "I'm child 1"), React.createElement("div", {id: "child", xyz: "abc"}, "I'm child 2")]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// // console.log(heading);
+// root.render(parent);      
+// chapter -1 - ends//  
