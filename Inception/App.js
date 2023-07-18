41620a5e463617c64ccb694f17a9f0ca6c113f20
Rakesh <mailto.dev.rakesh@gmail.com>
2023-07-18 14:31:49 +0530

    chapter 2 completed - igniting our app

--- Inception/App.js
+++ Inception/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const parent = React.createElement("div", {id: "parent", xyz: "abc "}, [React.createElement("div", {id: "child", xyz: "abc "}, "I'm child 1"), React.createElement("div", {id: "child", xyz: "abc"}, "I'm child 2")]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
