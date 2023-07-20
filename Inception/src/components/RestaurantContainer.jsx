394a0c13d85a68e8d83c9a00083c3e7c188b18b3
Rakesh <mailto.dev.rakesh@gmail.com>
2023-07-20 20:07:46 +0530

    chapter 5 completed

--- Inception/src/components/RestaurantContainer.jsx
+++ Inception/src/components/RestaurantContainer.jsx
@@ -1,12 +1,78 @@
 import RestaurantCard from "./RestaurantCard"
 import resData from "../utils/data"
 import { CDN_URL } from "../utils/constants"
+import { useState } from "react"
 
 const RestaurantContainer = () => {
 
+    const [restaurantsToRender, setRestaurantsToRender] = useState(resData)
+
+    // let restaurantsToRender = [
+    //     {
+    //         "info": {
+    //           "id": "580936",
+    //           "name": "EatFit",
+    //           "cloudinaryImageId": "94d3bc5e6392fbaf50686cf8cfab10ef",
+    //           "cuisines": [
+    //             "Healthy Food",
+    //             "Pizzas",
+    //             "North Indian",
+    //             "Thalis",
+    //             "Biryani",
+    //             "Chinese"
+    //           ],
+    //           "avgRating": 4.1,
+    //           "totalFee": 3700
+    //         }
+    //     },
+    //     {
+    //         "info": {
+    //           "id": "580937",
+    //           "name": "Dominos",
+    //           "cloudinaryImageId": "94d3bc5e6392fbaf50686cf8cfab10ef",
+    //           "cuisines": [
+    //             "Healthy Food",
+    //             "Pizzas",
+    //             "North Indian",
+    //             "Thalis",
+    //             "Biryani",
+    //             "Chinese"
+    //           ],
+    //           "avgRating": 4.7,
+    //           "totalFee": 2700
+    //         }
+    //     },
+    //     {
+    //         "info": {
+    //           "id": "580938",
+    //           "name": "Pizzahut",
+    //           "cloudinaryImageId": "94d3bc5e6392fbaf50686cf8cfab10ef",
+    //           "cuisines": [
+    //             "Healthy Food",
+    //             "Pizzas",
+    //             "North Indian",
+    //             "Thalis",
+    //             "Biryani",
+    //             "Chinese"
+    //           ],
+    //           "avgRating": 4.9,
+    //           "totalFee": 2700
+    //         }
+    //     }
+    // ];
+
     return (
         <div className="restaurant-container">
-            {resData.map((restaurant)=>(<RestaurantCard key={restaurant.info.id} title={restaurant.info.name} cuisine= {restaurant.info.cuisines.join(", ")} rating= {restaurant.info.avgRating} price= {restaurant.info.feeDetails.totalFee/10} img= {CDN_URL +  restaurant.info.cloudinaryImageId}/>))}
+            <button className="filter-btn-test" 
+            onClick={()=>{
+                let filteredData = restaurantsToRender.filter((item)=> item.info.avgRating > 4.2)
+                // console.log(restaurantsToRender)
+                setRestaurantsToRender(filteredData)
+            }}
+            > 
+            Filter Top Restaurants
+            </button>
+            {restaurantsToRender.map((restaurant)=>(<RestaurantCard key={restaurant.info.id} title={restaurant.info.name} cuisine= {restaurant.info.cuisines.join(", ")} rating= {restaurant.info.avgRating} price= {restaurant.info.totalFee/10} img= {CDN_URL +  restaurant.info.cloudinaryImageId}/>))}
         </div>
     )
 }
--- Inception/src/components/Search.jsx
+++ Inception/src/components/Search.jsx
@@ -1,6 +1,10 @@
 const Search = () => {
     return <div className="searchbar">
-            Search
+            <button className="filter-btn" 
+            onClick={(e)=>{}}
+            > 
+            Filter Top Restaurants
+            </button>
     </div>
 }
 
