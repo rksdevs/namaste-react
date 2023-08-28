6b76288c39d61217597073e734a44c26f1d4b2cb
Rakesh <mailto.dev.rakesh@gmail.com>
2023-08-28 15:07:51 +0530

    added css to filter btn

--- Inception/src/components/RestaurantContainer.jsx
+++ Inception/src/components/RestaurantContainer.jsx
@@ -15,7 +15,7 @@ const RestaurantContainer = () => {
     const fetchData = async () => {
         const data = await fetch ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9587847&lng=77.6999044&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
-        const json = await data.json();
+        const json = await data?.json();
         console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setRestaurantsToRender(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
